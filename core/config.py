ebb0fb0a4e5276a7e4c794e6f17199839feb0daf
miyazaki.carp <miyazaki.carp@gmail.com>
2020-11-23 00:50:06 +0900

    sample short training data

--- core/config.py
+++ core/config.py
@@ -26,7 +26,7 @@ __C.YOLO.IOU_LOSS_THRESH      = 0.5
 # Train options
 __C.TRAIN                     = edict()
 
-__C.TRAIN.ANNOT_PATH          = "./data/dataset/trafficlights.txt"
+__C.TRAIN.ANNOT_PATH          = "./data/dataset/trafficlights_short.txt"
 __C.TRAIN.BATCH_SIZE          = 16
 # __C.TRAIN.INPUT_SIZE            = [320, 352, 384, 416, 448, 480, 512, 544, 576, 608]
 __C.TRAIN.INPUT_SIZE          = 320
@@ -42,7 +42,7 @@ __C.TRAIN.SECOND_STAGE_EPOCHS   = 2
 # TEST options
 __C.TEST                      = edict()
 
-__C.TEST.ANNOT_PATH           = "./data/dataset/trafficlights.txt"
+__C.TEST.ANNOT_PATH           = "./data/dataset/trafficlights_short.txt"
 __C.TEST.BATCH_SIZE           = 16
 __C.TEST.INPUT_SIZE           = 320
 __C.TEST.DATA_AUG             = False
